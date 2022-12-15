d11d4da8a4597c4e8bd4ac948446db5d0c170725
R7007R <117884377+R7007R@users.noreply.github.com>
2022-12-15 12:09:53 +0700

    Add files via upload

---
+++ Soal 1_UG13.py
@@ -0,0 +1,13 @@
+import turtle as tur
+s = tur.Screen
+
+#NAMA:Rendy Ananta Kristanto
+#NIM :71220840
+
+# YANG DIPAKAI: R 840 A = R840A
+
+tur.color('red')
+style = ('Times New Roman', 30, 'bold')
+tur.write('R840A', font=style, align='center')
+tur.hideturtle()
+tur.done()
